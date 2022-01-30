e48137f197dff38a3932f1cfe87c162f0df0c5d1
Arun Giri <AGiri09@my.fisk.edu>
2022-01-29 20:56:18 -0600

    initial commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail" )
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail" ),
+    path("read-later",views.ReadLaterview.as_view(), name="read-later")
 ]
--- blog/views.py
+++ blog/views.py
@@ -56,11 +56,17 @@ class Allpostview(ListView):
 
 class SinglePostView(View):
   def get(self,request,slug):
+    identified_post = Post.objects.get(slug=slug)
+    stored_posts = request.session.get("stored_posts")
+    if stored_posts is not None:
+      is_saved_for_later = identified_post.id in stored_posts
+    else:
+      is_saved_for_later = False
     
     identified_post = Post.objects.get(slug=slug)
     comment_form = CommentForm()
     comments = identified_post.comments.all().order_by("-id")[0:3]
-    return render(request, "blog/post_detail.html", {"post": identified_post, "form":comment_form,"comments":comments })
+    return render(request, "blog/post_detail.html", {"post": identified_post, "form":comment_form,"comments":comments,"saved_for_later":is_saved_for_later,"tags":identified_post.tag.all()})
   
   def post(self,request,slug):
     comment_form = CommentForm(request.POST)
@@ -76,8 +82,34 @@ class SinglePostView(View):
     return HttpResponseRedirect(reverse("post-detail", args=[slug]))
 
   
+class ReadLaterview(View):
+  def get(self,request):
+    stored_posts = request.session.get("stored_posts")
+    context = {}
 
-#      
+    if stored_posts is None or len(stored_posts) == 0:
+      context["posts"] = []
+      context["has_posts"] = False
+    else:
+      posts = Post.objects.filter(id__in=stored_posts)
+      context["posts"] = posts
+      context["has_posts"] = True
+    return render(request,"blog/stored-posts.html", context)
+
+  def post(self, request):
+    stored_posts = request.session.get("stored_posts")
+    if stored_posts is None:
+      stored_posts = []
+    post_id = int(request.POST["post_id"])
+
+    if post_id  not in stored_posts:
+      stored_posts.append(post_id)
+      
+    else:
+      stored_posts.remove(post_id)
+    request.session["stored_posts"] = stored_posts
+    return HttpResponseRedirect("/")
+#     
 # class Singlepostview(DetailView):
 #   template_name = "blog/post-detail.html"
 #   model = Post
