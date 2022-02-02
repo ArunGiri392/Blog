7371a97c8b8589dd76b2147901cd2c89aff2e210
Arun Giri <AGiri09@my.fisk.edu>
2022-02-01 21:40:38 -0600

    made changes

--- my_site/settings.py
+++ my_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--1eha!f)*mf_q4xpbm_%+0(%u+pd50fbsjbp&pf6lpq$ch_c8)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = getenv("IS_PRODUCTION", True)
-DEBUG =  False
+DEBUG =  True
 
 ALLOWED_HOSTS = [
     "blogwebsitearun.herokuapp.com",
